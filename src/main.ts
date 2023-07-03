5affa962bb06727b2eb90e553905ef73ce4f7eb7
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-03 10:36:31 -0300

    fix: add cors on create Nest app

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('/api');
   app.enableCors();
 
